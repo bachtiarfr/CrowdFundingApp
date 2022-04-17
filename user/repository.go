55d67078d41cde1dd09e85e8cd48807ae3273d1e
MI-Bachtiar <bachtiar@madeindonesia.com>
2022-04-17 15:38:12 +0700

    Upload avatar API : find user by id

--- user/repository.go
+++ user/repository.go
@@ -12,6 +12,8 @@ import (
 type Repository interface {
 	SaveUser(user User) (User, error)
 	GetUserByEmail(email string) (User, error)
+	GetUserByID(ID int) (User, error)
+	UpdateUser(user User) (User, error)
 }
 
 type repository struct {
@@ -35,5 +37,26 @@ func (r *repository) GetUserByEmail(email string) (User, error) {
 		message := fmt.Sprintf("User with email %s not found", email)
 		fmt.Println(message)
 	}
+	return user, nil
+}
+
+func (r *repository) GetUserByID(ID int) (User, error) {
+	var user User
+	err := r.db.Where("ID = ?", ID).First(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) UpdateUser(user User) (User, error) {
+	err := r.db.Save(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
 	return user, nil
 }
